041b720921715b9692a43ee894a7f463334e6180
Christopher Moon <cmoon1@stevens.edu>
2021-12-04 15:02:25 -0500

    Added resume uploading to firestore db

--- client/src/Components/UploadResume.js
+++ client/src/Components/UploadResume.js
@@ -1,55 +1,76 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import { Button } from "@mui/material";
-import { resumeUpload } from '../firebase/FirebaseFunctions';
-import { AuthContext } from '../firebase/Auth';
+import React, { useContext, useEffect, useState } from "react";
+import { resumeUpload } from "../firebase/FirebaseFunctions";
+import { AuthContext } from "../firebase/Auth";
 import "../App.css";
 import { getSeeker } from "../firebase/FirebaseFunctions";
-
-const path = require("path");
+import {
+	FormControl,
+	FormGroup,
+	Button,
+	CircularProgress,
+} from "@mui/material";
 
 function UploadResume(props) {
-    const { currentUser } = useContext(AuthContext);
-    const [resumeName, setResumeName] = useState("None");
+	const [loading, setLoading] = useState(false);
+	const { currentUser } = useContext(AuthContext);
+	const [resumeLink, setResumeLink] = useState("");
 
-    useEffect(() => {
-        async function fetchData() {
-            let user = await getSeeker(currentUser.uid);
-            setResumeName(user.resume);
-        }
-        fetchData();
-    }, []);
+	useEffect(() => {
+		async function fetchData() {
+			let user = await getSeeker(currentUser.uid);
+			setResumeLink(user.resume);
+		}
+		fetchData();
+	}, [currentUser]);
 
+	const handleChange = async (e) => {
+		let resumeUrl = e.target.files[0];
+		try {
+			setLoading(true);
+			let postedUrl = await resumeUpload(currentUser.uid, resumeUrl);
+			setResumeLink(postedUrl);
+			setLoading(false);
+		} catch (e) {
+			setLoading(false);
+			console.log(e);
+		}
+	};
 
-    async function uploadResume() {
-        try {
-            const inputVal = document.getElementById("file").files[0].name;
-            let fileName = path.basename(inputVal);
-            await resumeUpload(currentUser.uid, fileName);
-            setResumeName(fileName);
-        }
-        catch (e) {
-            let error = document.getElementById("uploadError");
-            error.innerHTML = "Please upload a proper file.";
-        }
-    }
-
-    return (
-        <div>
-            <h3>Upload Resume</h3>
-            <h5>
-                Current Resume: {resumeName}
-            </h5>
-            <br />
-            <form>
-                <label className="button">
-                    <input type="file" id="file" accept="application/msword, application/pdf" />
-                </label><br />
-                <Button onClick={uploadResume}>Upload</Button>
-                <p id="uploadError"></p>
-            </form>
-        </div>
-    )
-    
+	return (
+		<div>
+			<h3>Upload Resume</h3>
+			<h5>
+				Current Resume:{" "}
+				{resumeLink ? (
+					<div>
+						<a href={resumeLink} target="_blank" rel="noreferrer">
+							Resume
+						</a>
+					</div>
+				) : (
+					<div></div>
+				)}
+			</h5>
+			<br />
+			{loading ? <CircularProgress /> : <div></div>}
+			<br />
+			<FormControl>
+				<FormGroup>
+					<Button variant="contained" component="label" disabled={loading}>
+						{resumeLink ? "Upload a different resume" : "Upload your resume"}
+						<input
+							type="file"
+							id="file"
+							name="resumeUrl"
+							hidden
+							accept="application/msword, application/pdf"
+							onChange={(e) => handleChange(e)}
+						/>
+					</Button>
+				</FormGroup>
+			</FormControl>
+		</div>
+	);
 }
 
 export default UploadResume;
